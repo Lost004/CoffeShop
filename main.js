dfaa9f4714ddb537c77a38a2c6b36915055b57b2
Lost004 <nazimri@code.edu.com>
2023-04-04 15:45:28 -0700

    ders

---
+++ main.js
@@ -0,0 +1,8 @@
+"use strict";
+
+let icon = document.querySelector('.icn')
+let shop = document.querySelector('.shop')
+
+shop.addEventListener('click',()=>{
+    shop.classList.toggle('active')
+})
